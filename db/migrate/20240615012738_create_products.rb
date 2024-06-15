4fd3cef449236ef29a3c96b17b1618eea0ee2de8
juaana <jjuanazabaleta@gmail.com>
2024-06-15 00:14:11 -0300

    Creacion del modelo, creacion de tabla y seedeo de tabla

---
+++ db/migrate/20240615012738_create_products.rb
@@ -0,0 +1,11 @@
+rclass CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+Product.destroy_all
+
+products = [
+  { title: 'Producto 1', description: 'Descripción del Producto 1', price: 19.99 },
+  { title: 'Producto 2', description: 'Descripción del Producto 2', price: 29.99 },
+  { title: 'Producto 3', description: 'Descripción del Producto 3', price: 39.99 },
+  { title: 'Producto 4', description: 'Descripción del Producto 4', price: 49.99 },
+  { title: 'Producto 5', description: 'Descripción del Producto 5', price: 59.99 },
+  { title: 'Producto 6', description: 'Descripción del Producto 5', price: 59.99 }
+]
+
+products.each do |product|
+  Product.create(product)
+end
+
+puts "Se han creado #{Product.count} productos"
