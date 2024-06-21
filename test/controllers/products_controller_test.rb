672448d4fa3bf905c648e6f48c20f7f00808318d
juaana <jjuanazabaleta@gmail.com>
2024-06-21 00:05:45 -0300

    Creación de formulario de producto y su respectivo test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,4 +14,10 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
         assert_select '.description', 'MyText1'
         assert_select '.price', '$9.99'
     end
+
+    test 'render a new product form' do
+        get new_product_path
+        assert response :success
+        assert_select 'form'
+    end
 end
