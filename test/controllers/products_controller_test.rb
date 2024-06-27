55ff5e85cc8b3f80a3bb0cd8474a7faa4253e0cd
juaana <jjuanazabaleta@gmail.com>
2024-06-26 22:48:16 -0300

    Se agrego el aviso de que se ha creado un nuevo producto

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -31,6 +31,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
             }
         }
         assert_redirected_to products_path
+        assert_equal flash[:notice], 'Tu producto se ha creado correctamente'
     end
     test 'does not allow to create a new product with empty fields' do 
         post products_path, params: {
